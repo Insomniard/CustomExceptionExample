d8977d24483838dddeb7fe126d4a47c15e305016
jjeong <psmb2006@naver.com>
2021-09-16 19:08:42 +0900

    Init

---
+++ src/main/java/Practice/CustomExceptionExample/CustomExceptionExampleApplication.java
@@ -0,0 +1,14 @@
+package Practice.CustomExceptionExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomExceptionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomExceptionExampleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/Practice/CustomExceptionExample/CustomExceptionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package Practice.CustomExceptionExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomExceptionExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
