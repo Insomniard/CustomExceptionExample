1bff6144df5322ac905d801be44ab15c7b017df2
jjeong <psmb2006@naver.com>
2021-09-28 19:47:29 +0900

    User parameter

--- src/main/java/Practice/CustomExceptionExample/Account/entity/Member.java
+++ src/main/java/Practice/CustomExceptionExample/Account/entity/Member.java
@@ -1,22 +1,32 @@
 package Practice.CustomExceptionExample.Account.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Member {
+@Builder
+public class Member implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private String memberId;
+    private String memberIds;
 
     @Column(nullable = false)
     private String memberName;
@@ -24,10 +34,57 @@ public class Member {
     @Column(nullable = false)
     private String memberEmail;
 
-    @Builder
-    public Member(String memberId, String memberName, String memberEmail) {
-        this.memberId = memberId;
+    @Column(nullable = false)
+    private String memberPassword;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+
+    public Member(Long id, String memberId, String memberName, String memberEmail, String memberPassword) {
+        this.id =  id;
+        this.memberIds = memberId;
         this.memberName = memberName;
         this.memberEmail = memberEmail;
+        this.memberPassword = memberPassword;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return memberPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return memberEmail;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
