7b4b6f4161e539cea64b2c5fcd6ca2b15df615f0
jjeong <psmb2006@naver.com>
2021-09-28 19:47:05 +0900

    UserDetail

--- src/main/java/Practice/CustomExceptionExample/Account/service/MemberService.java
+++ src/main/java/Practice/CustomExceptionExample/Account/service/MemberService.java
@@ -2,17 +2,24 @@ package Practice.CustomExceptionExample.Account.service;
 
 import Practice.CustomExceptionExample.Account.dto.MemberDto;
 import Practice.CustomExceptionExample.Account.dto.MemberJoinDto;
+import Practice.CustomExceptionExample.Account.entity.Member;
 import Practice.CustomExceptionExample.Account.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
-public class MemberService {
+public class MemberService implements UserDetailsService{
 
     private final MemberRepository memberRepository;
 
@@ -23,6 +30,12 @@ public class MemberService {
     @Transactional(readOnly = true)
     public List<MemberDto> findAll(){
         return memberRepository.findAll().stream().map(member -> new MemberDto(member)).collect(Collectors.toList());
-
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
+
 }
