89701ff9085b9b626328f7a6e154e3c6e005ba62
jjeong <psmb2006@naver.com>
2021-09-28 19:46:17 +0900

    jwt controller

--- src/main/java/Practice/CustomExceptionExample/Account/controller/RestControllerApi.java
+++ src/main/java/Practice/CustomExceptionExample/Account/controller/RestControllerApi.java
@@ -2,42 +2,46 @@ package Practice.CustomExceptionExample.Account.controller;
 
 import Practice.CustomExceptionExample.Account.dto.MemberDto;
 import Practice.CustomExceptionExample.Account.dto.MemberJoinDto;
+import Practice.CustomExceptionExample.Account.entity.Member;
+import Practice.CustomExceptionExample.Account.repository.MemberRepository;
 import Practice.CustomExceptionExample.Account.service.MemberService;
+import Practice.CustomExceptionExample.Account.util.TokenProvider;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/user")
 @RestController
 public class RestControllerApi {
 
     private final MemberService memberService;
 
-    @GetMapping("")
-    public Long save(@RequestBody MemberJoinDto memberJoinDto){
-        return memberService.save(memberJoinDto);
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
 
+    @PostMapping("/join")
+    public Long join(@RequestBody Map<String, String> member) {
+        return memberRepository.save(Member.builder()
+                .memberEmail(member.get("email"))
+                .memberPassword(passwordEncoder.encode(member.get("password")))
+                .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
+                .build()).getId();
     }
 
-    @GetMapping("/list")
-    public List<MemberDto> findAll(){
-        return memberService.findAll();
-    }
-
-    @GetMapping("/agree")
-    public Object agreeTestApi()throws Exception{
-        return memberService.agreetest();
-    }
-
-    @GetMapping("/")
-    public Object denyTestApi()throws Exception{
-        return memberService.denytest();
+    @PostMapping("/login")
+    public String login(@RequestBody Map<String, String> user) {
+        Member member = memberRepository.findByEmail(user.get("email"))
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+        return jwtTokenProvider.generateToken(member.getUsername(), member.getRoles());
     }
 
 }
