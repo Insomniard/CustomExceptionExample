71a830c22f76a38c1f433fb089c066303b9701d3
jjeong <psmb2006@naver.com>
2021-09-29 20:34:14 +0900

    JPA Email

--- src/main/java/Practice/CustomExceptionExample/Account/repository/MemberRepository.java
+++ src/main/java/Practice/CustomExceptionExample/Account/repository/MemberRepository.java
@@ -2,9 +2,11 @@ package Practice.CustomExceptionExample.Account.repository;
 
 import Practice.CustomExceptionExample.Account.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.Optional;
 
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String memberEmail);
 }
